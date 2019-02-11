fc4e7524204abd5f1b1b49e4ab4ae12a756e8d16
Razvan Florescu <florescurazvan5@gmail.com>
2019-02-11 23:18:04 +0200

    change the designe

--- TakeItEasyProject/TakeItEasyProject/ClientApp/src/app/app.module.ts
+++ TakeItEasyProject/TakeItEasyProject/ClientApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule }   from '@angular/forms';
-import { HeaderSideModule } from './header-side/header-side.module'
+import { HeaderSideModule } from './header-side/header-side.module';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

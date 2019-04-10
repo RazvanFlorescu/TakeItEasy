7715eed85f54dc1e30ca55508cd7d3fb8e2db902
Razvan Florescu <florescurazvan5@gmail.com>
2019-04-10 22:42:29 +0300

    add footer

--- TakeItEasyProject/TakeItEasyProject/ClientApp/src/app/home/home.component.ts
+++ TakeItEasyProject/TakeItEasyProject/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { LocationService } from './../shared/services/location.service';
 import { OnInit, Component } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,20 @@ export class HomeComponent implements OnInit {
    public constructor(private locationService: LocationService) {}
 
    public ngOnInit() { 
-        this.locationService.getCurrentLocation();
+        //this.locationService.getCurrentLocation();
+        $('.carousel .carousel-item').each(function(){
+          var next = $(this).next();
+          if (!next.length) {
+          next = $(this).siblings(':first');
+          }
+          next.children(':first-child').clone().appendTo($(this));
+          
+          if (next.next().length>0) {
+          next.next().children(':first-child').clone().appendTo($(this));
+          }
+          else {
+            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+          }
+      });
    }
 }
--- TakeItEasyProject/TakeItEasyProject/ClientApp/src/app/shared/shared.module.ts
+++ TakeItEasyProject/TakeItEasyProject/ClientApp/src/app/shared/shared.module.ts
@@ -4,14 +4,15 @@ import { LocationService } from './services/location.service';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [MenuComponent, HeaderComponent],
+  declarations: [MenuComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [MenuComponent, HeaderComponent],
+  exports: [MenuComponent, HeaderComponent, FooterComponent],
   providers: [LocationService]
 })
 export class SharedModule { }

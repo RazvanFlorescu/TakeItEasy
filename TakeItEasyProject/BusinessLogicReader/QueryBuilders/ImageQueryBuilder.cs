57cc6c2442c985853cbdb76ded570634290459ac
Razvan Florescu <razvan.florescu@maxcode.net>
2019-05-10 18:06:54 +0300

    add tabs on my profile page

--- TakeItEasyProject/BusinessLogicReader/QueryBuilders/ImageQueryBuilder.cs
+++ TakeItEasyProject/BusinessLogicReader/QueryBuilders/ImageQueryBuilder.cs
@@ -4,17 +4,17 @@ namespace BusinessLogicReader.QueryBuilders
 {
     public static class ImageQueryBuilder
     {
-        private const string getAllQuery = "SELECT * FROM ( SELECT EntityId, MAX(LastChangedDate) AS HIGHERDATE from Images  group by EntityId) m JOIN Images u on " +
+        private const string GetAllQuery = "SELECT * FROM ( SELECT EntityId, MAX(LastChangedDate) AS HIGHERDATE from Images  group by EntityId) m JOIN Images u on " +
                                            "m.HIGHERDATE = u.LastChangedDate and m.EntityId = u.EntityId where u.DeletedDate is null";
 
         public static string GetAll()
         {
-            return getAllQuery;
+            return GetAllQuery;
         }
 
         public static string GetByEntityId(Guid id)
         {
-            return getAllQuery + $" and u.EntityId = '{id}'";
+            return GetAllQuery + $" and u.EntityId = '{id}'";
         }
     }
 }
--- TakeItEasyProject/BusinessLogicReader/QueryBuilders/UserQueryBuilder.cs
+++ TakeItEasyProject/BusinessLogicReader/QueryBuilders/UserQueryBuilder.cs
@@ -6,27 +6,27 @@ namespace BusinessLogicReader.QueryBuilders
 {
     public static class UserQueryBuilder
     {
-        private const string getAllQuery = "SELECT * FROM ( SELECT EntityId, MAX(LastChangedDate) AS HIGHERDATE from Users  group by EntityId) m JOIN Users u on "+
+        private const string GetAllQuery = "SELECT * FROM ( SELECT EntityId, MAX(LastChangedDate) AS HIGHERDATE from Users  group by EntityId) m JOIN Users u on "+
                                            "m.HIGHERDATE = u.LastChangedDate and m.EntityId = u.EntityId where u.DeletedDate is null";
 
         public static string GetAll()
         {
-            return getAllQuery;
+            return GetAllQuery;
         }
 
         public static string GetByEntityId(Guid id)
         {
-            return getAllQuery + $" and u.EntityId = '{id}'";
+            return GetAllQuery + $" and u.EntityId = '{id}'";
         }
 
         public static string GetByEmail(string email)
         {
-            return getAllQuery + $" and u.Email = '{email}'";
+            return GetAllQuery + $" and u.Email = '{email}'";
         }
 
         public static string GetRegisteredUser(string email, string password)
         {
-            return getAllQuery + $" and u.Email = '{email}' and u.Password = '{password}'";
+            return GetAllQuery + $" and u.Email = '{email}' and u.Password = '{password}'";
         }
     }
 }
